4c5684eaf73aaaf9a5bfc4186a650181e04801b8
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2026-02-10 15:38:29 +0530

    renamed folder structure

---
+++ src/hello_world/tests/test_main.py
@@ -0,0 +1,40 @@
+
+from unittest.mock import Mock
+
+import pytest
+from hello_world import main
+
+def test_hello_http_with_json_name():
+    """
+    Method to test with JSON object
+    """
+    request = Mock(get_json=Mock(return_value={"name": "Alice"}), args=None)
+    response = main.hello_http(request)
+    assert "Hello, Alice!" in response
+
+def test_hello_http_with_args_name():
+    """
+    Mock a Request object with URL arguments (e.g. ?name=Bob)
+    """
+    request = Mock(get_json=Mock(return_value=None), args={"name": "Bob"})
+    response = main.hello_http(request)
+    assert "Hello, Bob!" in response
+
+def test_hello_http_default():
+    """
+    Mock a Request object with no data
+    """
+    request = Mock(get_json=Mock(return_value=None), args={})
+    response = main.hello_http(request)
+    assert "Hello, World!" in response
+
+def test_hello_http():
+    """
+    Mock a Request object with data
+    """
+    request = Mock()
+    request.get_json.return_value = {"name": "CI/CD"}
+    request.args = {}
+
+    response = main.hello_http(request)
+    assert "Hello, CI/CD!" in response
